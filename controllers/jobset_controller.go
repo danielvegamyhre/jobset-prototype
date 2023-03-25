9e4cfb1783f9b5be5890c23ebdb26e59aed3aeb0
Daniel Vega-Myhre <danielvm@google.com>
2023-03-25 00:12:17 +0000

    update controller

--- controllers/jobset_controller.go
+++ controllers/jobset_controller.go
@@ -40,6 +40,7 @@ import (
 type JobSetReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	Clock
 }
 
 var (
@@ -47,6 +48,16 @@ var (
 	apiGVStr                = jobsetv1.GroupVersion.String()
 	scheduledTimeAnnotation = "batch.tutorial.kubebuilder.io/scheduled-at"
 )
+
+type realClock struct{}
+
+func (_ realClock) Now() time.Time { return time.Now() }
+
+// clock knows how to get the current time.
+// It can be used to fake out timing for testing.
+type Clock interface {
+	Now() time.Time
+}
 
 //+kubebuilder:rbac:groups=batch.tutorial.kubebuilder.io,resources=jobsets,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=batch.tutorial.kubebuilder.io,resources=jobsets/status,verbs=get;update;patch
@@ -137,13 +148,37 @@ func (r *JobSetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 			log.V(1).Info("created Job for JobSet run", "job", job)
 		}
 	}
-	return ctrl.Result{RequeueAfter: r.Now()}, nil
+	return ctrl.Result{RequeueAfter: time.Millisecond}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *JobSetReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// set up a real clock, since we're not in a test
+	if r.Clock == nil {
+		r.Clock = realClock{}
+	}
+
+	if err := mgr.GetFieldIndexer().IndexField(context.Background(), &batchv1.Job{}, jobOwnerKey, func(rawObj client.Object) []string {
+		// grab the job object, extract the owner...
+		job := rawObj.(*batchv1.Job)
+		owner := metav1.GetControllerOf(job)
+		if owner == nil {
+			return nil
+		}
+		// ...make sure it's a CronJob...
+		if owner.APIVersion != apiGVStr || owner.Kind != "CronJob" {
+			return nil
+		}
+
+		// ...and if so, return it
+		return []string{owner.Name}
+	}); err != nil {
+		return err
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&jobsetv1.JobSet{}).
+		For(&batchv1.CronJob{}).
+		Owns(&batchv1.Job{}).
 		Complete(r)
 }
 
